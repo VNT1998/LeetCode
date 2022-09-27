566986fb1c6dd7895016f5e3eebe3dc040fb3ecb
Vinit Karkera <42674250+VNT1998@users.noreply.github.com>
2022-09-27 06:54:53 +0530

    Time: 1 ms, Memory: 42.1 MB - LeetHub

---
+++ pascals-triangle/pascals-triangle.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result  = new ArrayList();
+        List<Integer> row=new ArrayList();
+        for(int i=0;i<numRows;i++){
+            for(int j=row.size()-1;j>=1;j--){
+                row.set(j,row.get(j)+row.get(j-1));
+            }
+            row.add(1);
+            result.add(new ArrayList(row));
+        }
+        return result;
+    }
+}
